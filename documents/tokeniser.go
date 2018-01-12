28d1a81989d62a180f161323a035dd7baf04c290
bitterfly <dageneva@gmail.com>
2018-01-12 16:40:58 +0200

    Mega test for tokenising

--- documents/tokeniser.go
+++ documents/tokeniser.go
@@ -1,14 +1,39 @@
 package documents
 
-import "github.com/jdkato/prose/tokenize"
+import (
+	"strings"
+	"unicode"
 
-func TokeniseSentence(sentence string) [][]byte {
+	"github.com/jdkato/prose/tokenize"
+)
+
+func notPunctuation(word string) bool {
+	if len(word) == 0 {
+		return false
+	}
+
+	word = strings.TrimLeftFunc(word, func(r rune) bool { return r == '\'' })
+
+	for _, symbol := range word {
+		if symbol == '-' {
+			continue
+		}
+
+		if !unicode.IsLetter(symbol) {
+			return false
+		}
+	}
+	return true
+}
+
+func tokeniseSentence(sentence string) [][]byte {
 	tokeniser := tokenize.NewTreebankWordTokenizer()
 
-	tokens := make([][]byte, 1)
-
+	tokens := make([][]byte, 0)
 	for _, word := range tokeniser.Tokenize(sentence) {
-		tokens = append(tokens, []byte(word))
+		if notPunctuation(word) {
+			tokens = append(tokens, []byte(word))
+		}
 	}
 
 	return tokens
@@ -18,10 +43,11 @@ func Tokenise(text string) [][]byte {
 	sentenceSplitter, _ := tokenize.NewPragmaticSegmenter("en")
 	sentences := sentenceSplitter.Tokenize(text)
 
-	tokens := make([][]byte, len(sentences))
+	tokens := make([][]byte, 0)
 
 	for _, sentence := range sentences {
-		tokens = append(tokens, TokeniseSentence(sentence)...)
+
+		tokens = append(tokens, tokeniseSentence(sentence)...)
 	}
 
 	return tokens
