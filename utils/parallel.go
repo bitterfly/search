ca52a6baa6498cbaac3acb792bb4f0030e563253
bitterfly <dageneva@gmail.com>
2018-01-13 11:43:30 +0200

    Rename do function to work

--- utils/parallel.go
+++ utils/parallel.go
@@ -5,28 +5,28 @@ import (
 	"sync"
 )
 
-func Parallel(do func(), workers int) {
+func Parallel(work func(), workers int) {
 	wg := &sync.WaitGroup{}
 	wg.Add(workers)
 	for i := 0; i < workers; i++ {
 		go func() {
-			do()
+			work()
 			wg.Done()
 		}()
 	}
 	wg.Wait()
 }
 
-func ParallelCheck(do func() error, workers int, errors chan<- error) {
+func ParallelCheck(work func() error, workers int, errors chan<- error) {
 	Parallel(func() {
-		err := do()
+		err := work()
 		if err != nil {
 			errors <- err
 		}
 	}, workers)
 }
 
-func ParallelLog(do func() error, workers int) {
+func ParallelLog(work func() error, workers int) {
 	errors := make(chan error)
 	go func() {
 		for err := range errors {
@@ -34,6 +34,6 @@ func ParallelLog(do func() error, workers int) {
 		}
 	}()
 
-	ParallelCheck(do, workers, errors)
+	ParallelCheck(work, workers, errors)
 	close(errors)
 }
