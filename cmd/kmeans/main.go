c7306f6122682e5bad1d6ff03a0f79d5187dfaa7
bitterfly <dageneva@gmail.com>
2018-01-15 15:34:44 +0200

    Add main for kmeans

---
+++ cmd/kmeans/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/DexterLB/search/indices"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "kmeans"
+	app.Usage = "k-means"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "input, i",
+			Usage: "File with index",
+			Value: "/tmp/index.gob.gz",
+		},
+
+		cli.IntFlag{
+			Name:  "k",
+			Usage: "The parameter of k means",
+			Value: 3,
+		},
+	}
+
+	app.Action = mainCommand
+
+	app.Run(os.Args)
+}
+
+func mainCommand(c *cli.Context) {
+	ti := indices.NewTotalIndex()
+	err := ti.DeserialiseFromFile(c.String("input"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ti.Normalise()
+
+	log.Printf("k: %d\n", c.Int("k"))
+}
