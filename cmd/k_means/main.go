2f8c6fc8ac2c24a37584667a2f9e479cc13dac7e
bitterfly <dageneva@gmail.com>
2018-02-06 12:03:51 +0200

    Print sorted classes in documents cluster and print document info

--- cmd/k_means/main.go
+++ cmd/k_means/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 
 	"github.com/bitterfly/search/documents"
 	"github.com/bitterfly/search/indices"
@@ -62,7 +63,41 @@ func mainCommand(c *cli.Context) {
 	docInfo := processing.Count(documents[0], tokeniser)
 	i := kmeans.ClosestCentroidToInfo(ti, docInfo)
 
-	purity, classes := kmeans.Purity(ti, len((*ti).Centroids))
+	purity, allClasses := kmeans.Purity(ti, len((*ti).Centroids))
 
-	fmt.Printf("Purity %.3f\nDocuments is in cluster %d with most common class %s\n", purity, i, ti.ClassNames.GetInverse(classes[i]))
+	fmt.Printf("Purity %.3f\n\nDocument: %s\nis in cluster %d with most common classes:\n", purity, docInfo, i)
+	clusterClasses := sortDict(allClasses[i])
+
+	for i := 0; i < min(8, len(clusterClasses)); i++ {
+		fmt.Printf("%s\n", ti.ClassNames.GetInverse(clusterClasses[i]))
+	}
+}
+
+type IndexCount struct {
+	index int32
+	count int
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
+
+func sortDict(dict map[int32]int) []int32 {
+	classesCounts := make([]IndexCount, 0, len(dict))
+	for k, v := range dict {
+		classesCounts = append(classesCounts, IndexCount{index: k, count: v})
+	}
+
+	sort.Slice(classesCounts, func(i, j int) bool { return classesCounts[i].count > classesCounts[j].count })
+
+	classes := make([]int32, len(dict), len(dict))
+
+	for i := 0; i < len(classesCounts); i++ {
+		classes[i] = classesCounts[i].index
+	}
+
+	return classes
 }
